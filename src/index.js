38f24302b91755dc277289b060be9498f944717f
Ayo-Blackstar <ayo@blackstarmarketing.co>
2026-09-16 21:56:09 +0100

    Add EOD routes to the application

--- src/index.js
+++ src/index.js
@@ -11,10 +11,12 @@ app.use(express.urlencoded({ extended: true }));
 const payitmonthlyRoutes = require('./routes/payitmonthly');
 const ghlRoutes = require('./routes/ghl');
 const typeformRoutes = require('./routes/typeform');
+const eodRoutes = require('./routes/eod');
 
 app.use('/payitmonthly', payitmonthlyRoutes);
 app.use('/ghl', ghlRoutes);
 app.use('/typeform', typeformRoutes);
+app.use('/eod', eodRoutes);
 
 app.get('/', (req, res) => {
   res.json({ status: 'DBS Automation is running' });
